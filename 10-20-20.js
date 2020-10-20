cf9b21daa4d41a97ed3b7f461ef76e79fb4a4f05
Elizabeth Latimer <elizabeth.c.latimer@gmail.com>
2020-10-20 16:26:53 -0700

    plus one

--- 10-20-20.js
+++ 10-20-20.js
@@ -29,3 +29,24 @@ var lengthOfLastWord = function(s) {
   return wordsArr[wordsArr.length-1].length;
 
 };
+
+/**Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself. */
+
+var plusOne = function(digits) {
+  if(digits[0]===9) digits.unshift(0);
+  for (let i=digits.length-1; i>-1;i--){
+      if(digits[i]+1 < 10) {
+          digits[i] = digits[i] + 1;
+          break;
+      } else {
+          digits[i] = 0;
+          continue;
+      }
+  }
+  if(digits[0]===0) digits.shift();
+  return digits
+};
